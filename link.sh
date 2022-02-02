a4f086a011649cda1ec9cd9b45c150e5594a3c16
pgentili <nellorocca@gmail.com>
2022-02-02 14:19:56 +0100

    Simple script for first initialization

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ln -sf $(pwd)/.vimrc $HOME/.vimrc
+ln -sf $(pwd)/.vim $HOME/.vim
+
+ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
+
+ln -sf $(pwd)/zsh/.zshrc $HOME/.zshrc
+ln -sf $(pwd)/zsh/.zsh_alias $HOME/.zsh_alias
+
